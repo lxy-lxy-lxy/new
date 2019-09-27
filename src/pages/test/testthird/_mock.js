8479662b92b7900d81b6cd4c764e4a9e38889c09
lxy <2975741551@qq.com>
2019-09-27 17:14:52 +0800

    fifth

---
+++ src/pages/test/testthird/_mock.js
@@ -0,0 +1,146 @@
+import { parse } from 'url';
+// mock testListDataSource
+let testListDataSource = [];
+
+for (let i = 0; i < 8; i += 1) {
+  testListDataSource.push({
+    key: i,
+    disabled: i % 6 === 0,
+    href: 'https://ant.design',
+    avatar: [
+      'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
+      'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
+    ][i % 2],
+    name: `TradeCode ${i}`,
+    title: `一个任务名称 ${i}`,
+    owner: '曲丽丽',
+    aims: '这是一段描述',
+    callNo: Math.floor(Math.random() * 1000),
+    status: Math.floor(Math.random() * 10) % 4,
+    updatedAt: new Date(`2017-07-${Math.floor(i / 2) + 1}`),
+    createdAt: new Date(`2017-07-${Math.floor(i / 2) + 1}`),
+    progress: Math.ceil(Math.random() * 100),
+  });
+}
+
+function getRule(req, res, u) {
+  let url = u;
+
+  if (!url || Object.prototype.toString.call(url) !== '[object String]') {
+    // eslint-disable-next-line prefer-destructuring
+    url = req.url;
+  }
+
+  const params = parse(url, true).query;
+  let dataSource = testListDataSource;
+
+  if (params.sorter) {
+    const s = params.sorter.split('_');
+    dataSource = dataSource.sort((prev, next) => {
+      if (s[1] === 'descsend') {
+        return next[s[0]] - prev[s[0]];
+      }
+
+      return prev[s[0]] - next[s[0]];
+    });
+  }
+
+  if (params.status) {
+    const status = params.status.split(',');
+    let filterDataSource = [];
+    status.forEach(s => {
+      filterDataSource = filterDataSource.concat(
+        dataSource.filter(item => {
+          if (parseInt(`${item.status}`, 10) === parseInt(s.split('')[0], 10)) {
+            return true;
+          }
+
+          return false;
+        }),
+      );
+    });
+    dataSource = filterDataSource;
+  }
+
+  let pageSize = 10;
+
+  if (params.pageSize) {
+    pageSize = parseInt(`${params.pageSize}`, 0);
+  }
+
+  const result = {
+    list: dataSource,
+    pagination: {
+      total: dataSource.length,
+      pageSize,
+      current: parseInt(`${params.currentPage}`, 10) || 1,
+    },
+  };
+  return res.json(result);
+}
+
+function postRule(req, res, u, b) {
+  let url = u;
+
+  if (!url || Object.prototype.toString.call(url) !== '[object String]') {
+    // eslint-disable-next-line prefer-destructuring
+    url = req.url;
+  }
+
+  const body = (b && b.body) || req.body;
+  const { method, name, aims, key } = body;
+
+  switch (method) {
+    /* eslint no-case-declarations:0 */
+    case 'delete':
+      testListDataSource = testListDataSource.filter(item => key.indexOf(item.key) === -1);
+      break;
+
+    case 'post':
+      const i = Math.ceil(Math.random() * 10000);
+      testListDataSource.unshift({
+        key: i,
+        href: 'https://ant.design',
+        avatar: [
+          'https://gw.alipayobjects.com/zos/rmsportal/eeHMaZBwmTvLdIwMfBpg.png',
+          'https://gw.alipayobjects.com/zos/rmsportal/udxAbMEhpwthVVcjLXik.png',
+        ][i % 2],
+        name,
+        title: `一个任务名称 ${i}`,
+        owner: '曲丽丽',
+        aims,
+        callNo: Math.floor(Math.random() * 1000),
+        status: Math.floor(Math.random() * 10) % 2,
+        updatedAt: new Date(),
+        createdAt: new Date(),
+        progress: Math.ceil(Math.random() * 100),
+      });
+      break;
+
+    case 'update':
+      testListDataSource = testListDataSource.map(item => {
+        if (item.key === key) {
+          return { ...item, aims, name };
+        }
+
+        return item;
+      });
+      break;
+
+    default:
+      break;
+  }
+
+  const result = {
+    list: testListDataSource,
+    pagination: {
+      total: testListDataSource.length,
+    },
+  };
+  return res.json(result);
+}
+
+export default {
+  'GET /api/rule': getRule,
+  'POST /api/rule': postRule,
+};
